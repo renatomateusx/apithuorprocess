e5f87a6c7316035f5738b6059e99da0a2af9f49c
Renato Mateus <renatomateusx@gmail.com>
2020-05-25 22:15:36 -0300

    commit

--- routes/integracaoCampanhas/campanhas.js
+++ routes/integracaoCampanhas/campanhas.js
@@ -7,6 +7,8 @@ var campanhas = require('../../schemas/integracaoCampanhas/campanhas');
 /* User Pagination */
 //utilis.verifyJWT,
 router.post('/GetCampanhas', campanhas.GetCampanhas);
-router.post('/GetIntegracaoCampanhaByID', campanhas.GetCampanhaByID)
+router.post('/GetIntegracaoCampanhaByID', campanhas.GetCampanhaByID);
+router.post('/SaveCampanhaCartAbandon', campanhas.SaveCampanhaCartAbandon);
+router.post('/GetCampanhaCarrinhoAbandonado', campanhas.GetCampanhaCarrinhoAbandonado);
 
 module.exports = router;
