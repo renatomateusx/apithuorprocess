991ca006720a1ac6c8c11d6393c7f2482a9ef66e
Renato Mateus <renatomateusx@gmail.com>
2020-06-26 08:46:41 -0300

    commit

--- routes/extras.js
+++ routes/extras.js
@@ -1,16 +1,61 @@
 var express = require('express');
 var router = express.Router();
 var utilis = require('../resources/util');
-
-router.post('/ViaCEP', function(req, res, next) {
+var done = false;
+router.post('/ViaCEP', function (req, res, next) {
   try {
-    res.json(null);
     let LCep = req.body.cep;
     let URL = `https://viacep.com.br/ws/${LCep}/json/`;
+    /* SE DEMORAR MAIS DE 5 SEGUNDOS, É PORQUE NENHUMA ESTÁ FUNCIONANDO. */
+    setTimeout(() => { if (done == false) { done = true; res.json(null); res.end(); } }, 5000);
+    setTimeout(() => {
+      urlCEP2 = `http://cep.republicavirtual.com.br/web_cep.php?cep=${LCep}&formato=json`;
+      if (done == false) {
+        done = true;
+        utilis
+          .makeAPICallExternalHeadersCustom(urlCEP2, undefined, undefined)
+          .then(responseJSON => {
+
+            responseJSON = JSON.parse(responseJSON);
+            if (responseJSON.resultado == "1") {
+              var LCEP = {
+                "cep": responseJSON.cep,
+                "logradouro": responseJSON.tipo_logradouro + " " + responseJSON.logradouro,
+                "bairro": responseJSON.bairro,
+                "complemento": " ",
+                "localidade": responseJSON.cidade,
+                "uf": responseJSON.uf
+              }
+
+              res.json(LCEP);
+              res.end();
+            }
+
+          })
+          .catch(error => {
+            console.log('Erro ao pegar dados do Republica Virtual', error);
+          });
+      }
+    }, 2500);
     utilis
       .makeAPICallExternal(URL)
       .then(responseJSON => {
-        // res.json(responseJSON);
+        var LCEP = {
+          "cep": responseJSON.cep,
+          "logradouro": responseJSON.logradouro,
+          "complemento": responseJSON.complemento,
+          "bairro": responseJSON.bairro,
+          "localidade": responseJSON.localidade,
+          "uf": responseJSON.uf,
+          "unidade": responseJSON.unidade,
+          "ibge": responseJSON.ibge,
+          "gia": responseJSON.gia
+        }
+        if (done == false) {
+          done = true;
+          res.json(LCEP);
+          res.end();
+        }
       })
       .catch(error => {
         console.log('Erro ao pegar dados do VIACEP', error);
